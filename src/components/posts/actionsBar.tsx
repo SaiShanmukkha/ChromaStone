465c9f37e7b5c4ca4795fbd61eb6657863bcd080
Sai Shanmukkha Surapaneni <saishanmukkha@gmail.com>
2024-01-11 07:05:43 -0600

    v1.1.0

--- src/components/posts/actionsBar.tsx
+++ src/components/posts/actionsBar.tsx
@@ -10,10 +10,10 @@ export default function ActionsSideBar(){
 
     return (
         <aside className="bg-white sticky z-50 left-0 top-0 lg:top-40 px-2 py-6 lg:py-10 lg:m-1 lg:rounded-lg gap-6 w-full lg:w-1/12 h-fit flex flex-row lg:flex-col justify-around items-center">
-            <div className='flex flex-row lg:flex-col gap-3 items-center justify-center cursor-pointer' title='Add Reaction'>
+            {/* <div className='flex flex-row lg:flex-col gap-3 items-center justify-center cursor-pointer' title='Add Reaction'>
                 <span className='text-2xl'><MdOutlineAddReaction /></span>
                 <label className='text-md'>21</label>
-            </div>
+            </div> */}
             {/* <div className='flex flex-row lg:flex-col items-center justify-center gap-3 cursor-pointer' title='Go to Comments'>
                 <span className='text-2xl'><MdOutlineModeComment /></span>
                 <label className='text-md'>6</label>
