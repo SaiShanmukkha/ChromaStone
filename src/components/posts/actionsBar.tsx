8731cf061d04fcb0910a399d18dbcf2b37a53d0d
Sai Shanmukkha Surapaneni <saishanmukkha@gmail.com>
2024-01-11 07:04:58 -0600

    v1.0.0

--- src/components/posts/actionsBar.tsx
+++ src/components/posts/actionsBar.tsx
@@ -1,6 +1,13 @@
 import { MdOutlineAddReaction, MdBookmarkBorder, MdOutlineInsertLink, MdOutlineModeComment } from "react-icons/md";
 
 export default function ActionsSideBar(){
+
+    const handleCopyLink = ()=>{
+        navigator.clipboard.writeText(window.location.href)
+            .then(()=>alert("Blog Post Link Copied to clipboard"));
+    };
+
+
     return (
         <aside className="bg-white sticky z-50 left-0 top-0 lg:top-40 px-2 py-6 lg:py-10 lg:m-1 lg:rounded-lg gap-6 w-full lg:w-1/12 h-fit flex flex-row lg:flex-col justify-around items-center">
             <div className='flex flex-row lg:flex-col gap-3 items-center justify-center cursor-pointer' title='Add Reaction'>
@@ -11,11 +18,11 @@ export default function ActionsSideBar(){
                 <span className='text-2xl'><MdOutlineModeComment /></span>
                 <label className='text-md'>6</label>
             </div> */}
-            <div className='flex flex-row lg:flex-col gap-3 items-center justify-center cursor-pointer' title='Save'>
+            {/* <div className='flex flex-row lg:flex-col gap-3 items-center justify-center cursor-pointer' title='Save'>
                 <span className='text-2xl'><MdBookmarkBorder /></span>
                 <label className='text-md'>33</label>
-            </div>
-            <div className='cursor-pointer' title='Copy Link'>
+            </div> */}
+            <div className='cursor-pointer' title='Copy Link' onClick={handleCopyLink}>
                 <span className='text-2xl'><MdOutlineInsertLink /></span>
             </div>
       </aside>
